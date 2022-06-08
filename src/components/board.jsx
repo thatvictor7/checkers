43f9c1b635a11a313439b532f423f7b83706b37e
thatvictor7 <montoya.victor7@gmail.com>
2022-06-08 01:03:34 -0600

    styled board to be responsive

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,8 +2,11 @@ import '../styling/board.css'
 
 export default function Board() {
 
-    const rowNum = ['0', '1', '2', '3', '4', '5', '6', '7']
-    const columnChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    // const rowNum = ['0', '1', '2', '3', '4', '5', '6', '7']
+    const rowNum = [0,1,2,3,4,5,6,7]
+    // const columnChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    // const columnChar = ['0', '1', '2', '3', '4', '5', '6', '7']
+    const columnChar = [0,1,2,3,4,5,6,7]
 
     // function checker() {
     //     // for (let i = 0; i < 8; i++) {
@@ -17,13 +20,14 @@ export default function Board() {
     // }
 
     return (
-        <div id='board-container' className="flex-centered">
+        <div id='board-container' >
             {rowNum.map((val, index) => {
             // return <div key={index} className='square flex-centered'>{val}</div>
             return(
                 <div className='column'>
                     {columnChar.map((v,i) => {
-                    return <div className='square flex-centered'>{val} {v}</div>
+                        var res = val + v
+                    return <div className='square flex-centered'>{res}</div>
                 })}
                 </div>
             )
