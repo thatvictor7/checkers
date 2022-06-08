94a97252ef90e2f98b1b7cf31fe3498ee9808c6a
thatvictor7 <montoya.victor7@gmail.com>
2022-06-08 01:39:34 -0600

    added state and made checkerboard appearence on board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from 'react'
 import Board from './components/board.jsx'
 import './styling/App.css';
 
-function App() {
-  return (
-    <div className="App flex-centered">
+// function App() {
+//   return (
+//     <div className="App flex-centered">
+//       <Board />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: 1
+    }
+  }
+  
+  render() {
+    return(
+          <div className="App flex-centered">
       <Board />
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,5 +1,11 @@
 import '../styling/board.css'
 
+function colorSelector(num) {
+    return num % 2 === 0 ? 
+    <div key={num} style={{backgroundColor: "#3bdbff"}} className='square flex-centered'>{num}</div> : 
+    <div key={num} style={{backgroundColor: "#0478b7"}} className='square flex-centered'>{num}</div>
+}
+
 export default function Board() {
 
     // const rowNum = ['0', '1', '2', '3', '4', '5', '6', '7']
@@ -7,17 +13,6 @@ export default function Board() {
     // const columnChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     // const columnChar = ['0', '1', '2', '3', '4', '5', '6', '7']
     const columnChar = [0,1,2,3,4,5,6,7]
-
-    // function checker() {
-    //     // for (let i = 0; i < 8; i++) {
-    //     //     return(
-    //     //         <div>t</div>
-    //     //     )    
-    //     // }
-    //     rowNum.map((val, index) => {
-    //         return <div key={index} className='square'>{val}</div>
-    //     })
-    // }
 
     return (
         <div id='board-container' >
@@ -27,7 +22,8 @@ export default function Board() {
                 <div className='column'>
                     {columnChar.map((v,i) => {
                         var res = val + v
-                    return <div className='square flex-centered'>{res}</div>
+                    return colorSelector(res)
+                    // return <div key={res} className='square flex-centered'>{res}</div>
                 })}
                 </div>
             )
