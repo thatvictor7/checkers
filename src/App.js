47f7180e59d87d17e7cca51ab2f43b15c70a9891
thatvictor7 <montoya.victor7@gmail.com>
2022-06-09 22:17:12 -0600

    added checker positions to state and passed as props to board component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,29 @@ import React from 'react'
 import Board from './components/board.jsx'
 import './styling/App.css';
 
-// function App() {
-//   return (
-//     <div className="App flex-centered">
-//       <Board />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      data: 1
+      emptySpaces: ["25","27","29","31","34","36","38","40"],
+      lightCheckersPositions: {
+        al: "02", bl: "04", cl: "06", dl: "08",
+        el: "09", fl: "11", gl: "13", hl: "15",
+        il: "18", jl: "20", kl: "22", ll: "24"
+      },
+      darkCheckersPositions: {
+        ad: "02", bd: "04", cd: "06", dd: "08",
+        ed: "09", fd: "11", gd: "13", hd: "15",
+        id: "18", jd: "20", kd: "22", ld: "24"
+      }
     }
   }
-  
+
   render() {
     return(
           <div className="App flex-centered">
-      <Board />
+      <Board state={this.state} />
     </div>
     )
   }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,28 +1,34 @@
 import '../styling/board.css'
 
-function colorSelector(num) {
+function colorSelector(num, squareNum) {
+    let stringNumber = doubleDigit(squareNum)
     return num % 2 === 0 ? 
-    <div key={num} style={{backgroundColor: "#3bdbff"}} className='square flex-centered'>{num}</div> : 
-    <div key={num} style={{backgroundColor: "#0478b7"}} className='square flex-centered'>{num}</div>
+    <div key={squareNum} style={{backgroundColor: "#3bdbff"}} className='square flex-centered'>{stringNumber}</div> : 
+    <div key={squareNum} style={{backgroundColor: "#0478b7"}} className='square flex-centered'>{stringNumber}</div>
+    // <div key={squareNum} style={{backgroundColor: "#0478b7"}} className='square flex-centered'><div className='disk light'></div></div>
 }
 
-export default function Board() {
+function doubleDigit(number) {
+    let newNumber = number.toString()
+    return newNumber.length < 2 ? 
+    `0${newNumber}`:
+    newNumber;
+}
 
-    // const rowNum = ['0', '1', '2', '3', '4', '5', '6', '7']
+export default function Board(props) {
     const rowNum = [0,1,2,3,4,5,6,7]
-    // const columnChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    // const columnChar = ['0', '1', '2', '3', '4', '5', '6', '7']
     const columnChar = [0,1,2,3,4,5,6,7]
-
+    let counter = 0
+    console.log(props.state);
     return (
         <div id='board-container' >
             {rowNum.map((val, index) => {
-            // return <div key={index} className='square flex-centered'>{val}</div>
             return(
                 <div className='column'>
                     {columnChar.map((v,i) => {
+                        counter++
                         var res = val + v
-                    return colorSelector(res)
+                    return colorSelector(res,counter)
                     // return <div key={res} className='square flex-centered'>{res}</div>
                 })}
                 </div>
